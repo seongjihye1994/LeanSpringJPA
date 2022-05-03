67f7bf249a42ae1139acdd20a8338521ffb10d74
jihye.seong <jihye.seong@ipageon.com>
2022-05-04 08:16:30 +0900

    주문 및 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -72,6 +72,72 @@ public class Order {
         delivery.setOrder(this); // 배송 객체 생성 시 주문 연관관계 설정
     }
 
+    // == 생성 메서드 ==
+
+    /**
+     * 주문 생성
+     * @param member
+     * @param delivery
+     * @param orderItems
+     * @return
+     */
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) { // OrderItem... : 여러개의 OrderItem 파라미터를 넘길 수 있음.
+
+        Order order = new Order();
+
+        // 주문 한 사람
+        order.setMember(member);
+
+        // 배송
+        order.setDelivery(delivery);
+
+        // 주문 한 수 만큼 addOrderItem
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        // 주문 상태를 ORDER 로 강제
+        order.setStatus(OrderStatus.ORDER);
+
+        // 주문 시간 현재 시간으로 강제
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+
+    /**
+     * 주문 취소
+     */
+
+    public void cancel() {
+
+        // 만약 배송 상태가 이미 배송 완료라면 배송 취소 불가
+       if (delivery.getStatus() == DeliveryStatus.COMP) {
+           throw new IllegalStateException("이미 배송 완료 된 상품은 취소가 불가합니다.");
+        }
+
+       // 주문 상태 '취소'로
+       this.setStatus(OrderStatus.CANCEL);
+
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel(); // 각각의 주문에 주문 취소를 해야한다.
+        }
+    }
+
+    // == 조회 로직 == //
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+    }
+
+
 
 
 

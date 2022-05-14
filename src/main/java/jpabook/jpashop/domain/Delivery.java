1e7dbeed6988ec69419e886f6fe47160ec76b671
jihye.seong <jihye.seong@ipageon.com>
2022-05-14 21:44:43 +0900

    ãjpa 기본 어노테이션

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -20,7 +20,14 @@ public class Delivery {
     @Embedded // 임베디드 타입, DB에 테이블 생성 x
     private Address address;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) // java의 ENUM 타입 명시
     private DeliveryStatus status; // ENUM [READY(준비), COMP(배송)]
 
 }
+
+/**
+ * @Enumerated 사용 시 주의사항!
+ * Enum 타입의 디폴트인 오디널은 사용하지 말자. -> 순서대로 추가되는 형식은 추후 데이터가 변하면 순서가 엉망이 될 수 있다.
+ *
+ * 그냥 명시적으로 적어주는 String 타입을 사용하자!
+ */
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,7 +15,7 @@ public class Member {
 
     @Id // 엔티티 식별자
     @GeneratedValue // db 에서 pk 값 자동 생성 위임
-    @Column(name = "member_id") // PK 컬럼명 지정
+    @Column(name = "member_id") // PK 컬럼명 지정 -> 객체는 id, DB의 컬럼명은 member_id로 지정
     private Long id;
 
     @NotEmpty // @Valid 가 검증함. 만약 값이 비어있다면 에러
@@ -35,3 +35,15 @@ public class Member {
     private List<Order> orders = new ArrayList<>();
 
 }
+
+/**
+ * @Column 속성
+ *
+ * name - DB 컬럼명 지정 가능, 디폴트는 객체의 필드 이름
+ * insertable, updatable - 등록, 변경 가능 여부 -> 만약 false 로 두면 JPA 단에서는 절대 insert 되거나 update 되지 않는다.
+ * nullable - null 값의 허용 여부를 설정한다. false 로 설정하면 DDL 생성 시에 not null 제약조건이 붙는다.
+ * unique - @Table의 uniqueConstraints와 같지만 한 컬럼에 간단히 유니크 제약조건을 걸 때 사용한다. 잘 사용하지 않는다. 제약조건의 이름을 설정할 수 없기 때문, @Table uniqueConstraints 사용하자.
+ * columnDefinition - 데이터베이스 컬럼 정보를 직접 줄 수 있다. ex) varchar(100) default 'EMPTY'
+ * length - 문자 길이 제약조건, String 타입에만 사용한다. ex) length 를 10으로 설정하면 varchar 10 이 됨
+ * precision, scale - 빅데시멀 타입에서 사용한다.
+ */
